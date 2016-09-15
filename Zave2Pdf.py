bbdb1c44b25829732ee932781e70c2cacaa37f5d
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2016-09-15 23:18:18 +0300

    Zave2Pdf script and read file

---
+++ Zave2Pdf.py
@@ -0,0 +1,140 @@
+#! /usr/bin/env python
+
+import os, sys, zipfile, shutil
+import xml.etree.ElementTree as ET
+from reportlab.pdfgen import canvas
+from pdfrw import PdfReader, PdfWriter, PageMerge
+
+def usage():
+    print "\nMain usage is;"
+    print "    Zave2Pdf.py -zave input/folder/file.zave"
+    print "\nIn order to specify saving file directory, use;"
+    print "    Zave2Pdf.py -zave input/folder/file.zave -s save/path/file.pdf\n"
+
+parameters = sys.argv[1:]
+
+if not parameters.index("-zave")+1:
+    print parameters
+    print "you should indicate the zave file"
+    usage()
+    exit()
+else:
+    url = parameters[parameters.index("-zave") + 1]
+    zave = zipfile.ZipFile(url, "r")
+    url = os.path.dirname(url) + "/ZaveExtractedFiles"
+    os.mkdir(url)
+    zave.extractall(url)
+
+try:
+    if parameters.index("-s")+1:
+        save = parameters[parameters.index("-s") + 1]
+except:
+    save = os.path.dirname(url)+"/issue.pdf"
+
+if not os.path.isdir(url+"/Zave2PDFpages/"):
+    os.mkdir(url+"/Zave2PDFpages/")
+
+tree = ET.parse(url+ "/magazine.xml")
+
+pages = tree.findall("articles/article/layouts/layout/page")
+cnt = 1
+items=[]
+
+def addmedia(kind):
+    for i in kind:
+        try:
+            if i.attrib["isButton"]:
+                """Buttons couldn't be drawn"""
+                pass
+        except:
+            file = i.find("fileSource").attrib["path"]
+
+            if file[-4:-3] == ".":
+                x = i.attrib["x"]
+                y = i.attrib["y"]
+                height = i.attrib["height"]
+                width = i.attrib["width"]
+                items.append([int(x), int(y), int(height), int(width), file])
+
+print "\npage preparation ",
+for page in pages:
+    try:
+        images = page.findall("image")
+    except:
+        images = False
+
+    try:
+        pdfs = page.findall("pdf")
+    except:
+        pdfs = page.find("pdf")
+
+    if images:
+        addmedia(images)
+
+    sizes = page.attrib
+    sizes = [int(sizes["width"]), int(sizes["height"])]
+
+    c = canvas.Canvas(url+"/Zave2PDFpages/page"+str(cnt)+".pdf")
+    c.setPageSize((sizes[0], sizes[1]))
+
+    if items:
+        for i in items:
+            """
+            x: i[0] y: i[1]
+            w: i[3] h: i[2]
+            """
+            c.drawImage((url+"/"+i[4]), (i[0]), (sizes[1]-i[2]-i[1]), i[3], i[2], mask="auto")
+    else:
+        c.showPage()
+
+    if page.find("subLayout") is not None: #Special Page
+        sub = page.find("subLayout")
+        if sub.attrib["scrollDirection"] == "2":
+            width = int(sub.attrib["width"])
+            height = int(sub.attrib["height"])
+            x = int(sub.attrib["x"]) + width/2
+            y = int(sizes[1]) - int(sub.attrib["y"]) - height/2
+            c.drawCentredString(x, y, "Icerigi bir sonraki sayfada goruntuleyebilirsiniz.")#the Turkish message of "you can view the content at following page"
+            print "|#|",
+
+    c.save()
+    items=[]
+    addmedia(pdfs)
+    current_page = PdfReader(url+"/Zave2PDFpages/page" + str(cnt) + ".pdf")
+
+    for i in items[:]:
+        pdf_media_file = PdfReader(url+"/" + i[4])
+        pdf_media = PageMerge().add(pdf_media_file.pages[0])[0]
+        pdf_media.x = i[0]
+        pdf_media.y = sizes[1]-i[2]-i[1]
+        pdf_media.w = i[2]
+        pdf_media.h = i[3]
+
+        PageMerge(current_page.pages[0]).add(pdf_media).render()
+        PdfWriter().write((url+"/Zave2PDFpages/page"+str(cnt)+".pdf"), current_page)
+
+    items = []
+
+    sublayouts = page.findall("subLayout/internalSubLayout/subLayout/pages/page")
+    if sublayouts:
+        for sublayout in sublayouts:
+            pages.insert(cnt,sublayout)
+
+    if cnt % 5 == 0:
+        sys.stdout.write("#")
+        sys.stdout.flush()
+    cnt = cnt + 1
+
+print "\n\n" + str(cnt - 1) + " pages are being merged..."
+
+writer = PdfWriter()
+for i in range(1, cnt):
+    writer.addpages(PdfReader(url+"/Zave2PDFpages/page"+str(i)+".pdf").pages)
+
+writer.write(save)
+
+print "\npreparing document"
+print "\ndeleting temporary files and folders"
+shutil.rmtree(url)
+
+print "\nfile saved: "+ save
---
+++ readme.md
@@ -0,0 +1,56 @@
+#Zave2Pdf
+
+_Zave2Pdf_ is a Python script that, converts a zave file into a pdf file, by using [reportlab](https://pypi.python.org/pypi/reportlab), [pdfrw](https://github.com/pmaupin/pdfrw/) and Python xml library. To use this script, reportlab and pdfrw should be installed. And pdfrw requires a patch before install.
+
+__IMPORTANT__: Script requires to apply [developer's patch](https://github.com/pmaupin/pdfrw/pull/63) to pdfrw, because related pull request hasn't merged to master repository. For detailed information and application steps look at __Prepare pdfrw__ title.
+
+##Running Script
+
+Zave2Pdf work with two parameters; __-zave__ to indicate zave file and __-s__ to indicate (optional) output pdf file.
+
+Usage;
+
+     Zave2Pdf.py -zave /Path/of/ZaveFile.zave
+
+Indicating the save path and file name(optional);
+
+     Zave2Pdf.py -zave /Path/of/ZaveFile.zave -s /Save/Directory/FileName.pdf
+
+
+##Prepare pdfrw
+###Download pdfrw
+Original git repository of pdfrw master is [pmaupin/pdfrw/](https://github.com/pmaupin/pdfrw/). And clone url can be found from _Clone or Download_ button as [https://github.com/pmaupin/pdfrw.git](https://github.com/pmaupin/pdfrw.git)
+
+And related patch file can be found as [https://github.com/pmaupin/pdfrw/pull/63.patch](https://github.com/pmaupin/pdfrw/pull/63.patch)
+
+First go your local folder
+
+	cd /Users/user_name/Downloads/
+	
+An get pdfrw master,
+	
+	git clone https://github.com/pmaupin/pdfrw.git
+	
+Master is here. Now go into folder,
+	
+	cd pdfrw
+
+Create .patch file by _curl_, from github's patch file url ([_pmaupin/pdfrw/pull/63.patch_](https://patch-diff.githubusercontent.com/raw/pmaupin/pdfrw/pull/63.patch)) which can be found at [63rd pull request page.](https://github.com/pmaupin/pdfrw/pull/63)
+
+	curl -0 https://patch-diff.githubusercontent.com/raw/pmaupin/pdfrw/pull/63.patch
+	
+And apply patch file,
+
+	git apply 63.patch
+
+After patch, pdfrw is ready!
+
+###Install pdfrw
+
+	python setup.py build
+	sudo python setup.py install
+     
+##Some Notes
+
+- Zave files can consist scrollable contents. But pdfs can't, in traditional ways. Zave2Pdf adds scrollable content as following page and prints a message into scroll area as "You can view the content at the following page"
+- This script based on zave files which contains pdf and png files. This kind of zave files is the majortiy of zave files. When any problems occured, please use issues or send an e-mail to me.
